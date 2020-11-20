5c5b320a78c7bad6f0328fb5d75792dd9cc7840d
Oleg Boiko <oboiko@chegg.com>
2020-11-20 13:11:41 -0800

    aligning cpu training script with gpu one

--- ddp_tutorial_cpu.py
+++ ddp_tutorial_cpu.py
@@ -8,9 +8,8 @@ from tqdm import tqdm
 
 DISABLE_TQDM = True
 
-def create_data_loaders(rank: int,
-                        world_size: int,
-                        batch_size: int) -> Tuple[DataLoader, DataLoader]:
+
+def create_data_loaders(batch_size: int) -> Tuple[DataLoader, DataLoader]:
     transform = transforms.Compose([
         transforms.ToTensor(),
         transforms.Normalize((0.1307,), (0.3081,))
@@ -40,52 +39,33 @@ def create_data_loaders(rank: int,
 
     return train_loader, test_loader
 
-def main():
-    # download train dataset
-    train_dataset = datasets.MNIST('./mnist_data',
-                                   download=True,
-                                   train=True,
-                                   transform=transforms.Compose([
-                                       transforms.ToTensor(),
-                                       transforms.Normalize((0.1307,), (0.3081,))
-                                   ]))
-    # download test dataset
-    test_dataset = datasets.MNIST('./mnist_data',
-                                  download=True,
-                                  train=False,
-                                  transform=transforms.Compose([
-                                      transforms.ToTensor(),
-                                      transforms.Normalize((0.1307,), (0.3081,))
-                                  ]))
-    # create train data loader
-    train_loader = torch.utils.data.DataLoader(train_dataset,
-                                               batch_size=128,
-                                               shuffle=True,
-                                               num_workers=4)
-    # create test data loader
-    test_loader = torch.utils.data.DataLoader(test_dataset,
-                                              batch_size=128,
-                                              shuffle=True,
-                                              num_workers=4)
+
+def create_model():
     # create model architecture
     model = nn.Sequential(
-        nn.Linear(28 * 28, 128),
+        nn.Linear(28*28, 128),  # MNIST images are 28x28 pixels
         nn.ReLU(),
         nn.Dropout(0.2),
         nn.Linear(128, 128),
         nn.ReLU(),
-        nn.Linear(128, 10, bias=False)
+        nn.Linear(128, 10, bias=False)  # 10 classes to predict
     )
+    return model
 
+
+def main(model: nn.Module,
+         train_loader: DataLoader,
+         test_loader: DataLoader) -> nn.Module:
     # initialize optimizer and loss function
     optimizer = optim.SGD(model.parameters(), lr=0.01)
     loss = nn.CrossEntropyLoss()
 
     # train the model
     for i in range(10):
+        model.train()
         epoch_loss = 0
         # train the model for one epoch
-        pbar = tqdm(train_loader, disable=DISABLE_TQDM)
+        pbar = tqdm(train_loader)
         for x, y in pbar:
             x = x.view(x.shape[0], -1)
             optimizer.zero_grad()
@@ -93,29 +73,33 @@ def main():
             batch_loss = loss(y_hat, y)
             batch_loss.backward()
             optimizer.step()
-
             batch_loss_scalar = batch_loss.item()
             epoch_loss += batch_loss_scalar / x.shape[0]
             pbar.set_description(f'training batch_loss={batch_loss_scalar:.4f}')
 
         # calculate validation loss
         with torch.no_grad():
+            model.eval()
             val_loss = 0
-            pbar = tqdm(test_loader, disable=DISABLE_TQDM)
+            pbar = tqdm(test_loader)
             for x, y in pbar:
                 x = x.view(x.shape[0], -1)
                 y_hat = model(x)
                 batch_loss = loss(y_hat, y)
-
                 batch_loss_scalar = batch_loss.item()
+
                 val_loss += batch_loss_scalar / x.shape[0]
                 pbar.set_description(f'validation batch_loss={batch_loss_scalar:.4f}')
 
         print(f"Epoch={i}, train_loss={epoch_loss:.4f}, val_loss={val_loss:.4f}")
 
-    return model
+    return model.module
 
 
 if __name__ == '__main__':
-    model = main()
-    # serialize and publish the model etc.
+    train_loader, test_loader = create_data_loaders(128)
+    model = main(model=create_model(),
+                 train_loader=train_loader,
+                 test_loader=test_loader)
+
+    torch.save(model.state_dict(), 'model.pt')
