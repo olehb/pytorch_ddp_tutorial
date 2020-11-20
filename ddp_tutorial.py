4550814c505a732e5c63ae2bda5d91b4fc5489a8
Oleg Boiko <oboiko@chegg.com>
2020-11-19 17:57:26 -0800

    Adapting the script for DDP

--- ddp_tutorial.py
+++ ddp_tutorial.py
@@ -1,35 +1,53 @@
+import argparse
+from typing import Tuple
+
 import torch
-from torchvision import datasets, transforms
 from torch import nn, optim
+from torch.distributed import Backend
+from torch.nn.parallel.distributed import DistributedDataParallel
+from torch.utils.data import DataLoader, DistributedSampler
+from torchvision import datasets, transforms
 
 
-def main():
-    # download train dataset
-    train_dataset = datasets.MNIST('./mnist_data',
+def create_data_loaders(rank: int,
+                        world_size: int,
+                        batch_size: int) -> Tuple[DataLoader, DataLoader]:
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.1307,), (0.3081,))
+    ])
+    dataset_loc = './mnist_data'
+
+    train_dataset = datasets.MNIST(dataset_loc,
                                    download=True,
                                    train=True,
-                                   transform=transforms.Compose([
-                                       transforms.ToTensor(),
-                                       transforms.Normalize((0.1307,), (0.3081,))
-                                   ]))
-    # download test dataset
-    test_dataset = datasets.MNIST('./mnist_data',
+                                   transform=transform)
+    sampler = DistributedSampler(train_dataset,
+                                 num_replicas=world_size,  # Number of GPUs
+                                 rank=rank,  # GPU where process is running
+                                 shuffle=True,  # Shuffling is done by Sampler
+                                 seed=42)
+    train_loader = DataLoader(train_dataset,
+                              batch_size=batch_size,
+                              shuffle=False,  # This is mandatory to set this to False here, shuffling is done by Sampler
+                              num_workers=0,  # This is important to load data in the same process, so it should be 0
+                              sampler=sampler,
+                              pin_memory=True)
+
+    # This is not necessary to use distributed sampler for the test or validation sets.
+    test_dataset = datasets.MNIST(dataset_loc,
                                   download=True,
                                   train=True,
-                                  transform=transforms.Compose([
-                                      transforms.ToTensor(),
-                                      transforms.Normalize((0.1307,), (0.3081,))
-                                  ]))
-    # create train data loader
-    train_loader = torch.utils.data.DataLoader(train_dataset,
-                                               batch_size=128,
-                                               shuffle=True,
-                                               num_workers=4)
-    # create test data loader
-    test_loader = torch.utils.data.DataLoader(test_dataset,
-                                              batch_size=128,
-                                              shuffle=True,
-                                              num_workers=4)
+                                  transform=transform)
+    test_loader = DataLoader(test_dataset,
+                             batch_size=batch_size,
+                             shuffle=True,
+                             num_workers=0)
+
+    return train_loader, test_loader
+
+
+def create_model():
     # create model architecture
     model = nn.Sequential(
         nn.Linear(28*28, 128),
@@ -39,6 +57,16 @@ def main():
         nn.ReLU(),
         nn.Linear(128, 10, bias=False)
     )
+    return model
+
+
+def main(rank: int,
+         model: nn.Module,
+         train_loader: DataLoader,
+         test_loader: DataLoader) -> nn.Module:
+    device = torch.device(f'cuda:{rank}')
+    model = model.to(device)
+    model = DistributedDataParallel(model, device_ids=[rank], output_device=rank)
 
     # initialize optimizer and loss function
     optimizer = optim.SGD(model.parameters(), lr=0.01)
@@ -46,9 +74,16 @@ def main():
 
     # train the model
     for i in range(10):
+        model.train()
+        train_loader.sampler.set_epoch(i)
+        test_loader.sampler.set_epoch(i)
+
         epoch_loss = 0
         # train the model for one epoch
         for x, y in train_loader:
+            x = x.to(device)
+            y = y.to(device)
+
             x = x.view(x.shape[0], -1)
             optimizer.zero_grad()
             y_hat = model(x)
@@ -59,8 +94,11 @@ def main():
 
         # calculate validation loss
         with torch.no_grad():
+            model.eval()
             val_loss = 0
             for x, y in test_loader:
+                x = x.to(device)
+                y = y.to(device)
                 x = x.view(x.shape[0], -1)
                 y_hat = model(x)
                 batch_loss = loss(y_hat, y)
@@ -68,9 +106,26 @@ def main():
 
         print(f"Epoch={i}, train_loss={epoch_loss}, val_loss={val_loss}")
 
-    return model
+    return model.module
 
 
 if __name__ == '__main__':
-    model = main()
-    # serialize and publish the model etc.
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--local_rank", type=int)
+    args = parser.parse_args()
+
+    rank = args.local_rank
+    world_size = torch.cuda.device_count()
+
+    torch.cuda.set_device(rank)
+    torch.distributed.init_process_group(backend=Backend.NCCL,
+                                         init_method='env://')
+
+    train_loader, test_loader = create_data_loaders(rank, world_size, 128)
+    model = main(rank=rank,
+                 model=create_model(),
+                 train_loader=train_loader,
+                 test_loader=test_loader)
+
+    if rank == 0:
+        model.save_pretrained('./output')
